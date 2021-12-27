d9b502fd1e8ac160209f85cba1feace6a2516621
Gustavonvp <gustavonvp@gmail.com>
2021-12-27 19:16:35 -0300

    card html and css with component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
 import { NgxMaskModule } from 'ngx-mask';
 import { IConfig } from 'ngx-mask';
+import { CardComponent } from './card/card.component';
 
 export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 
 @NgModule({
-  declarations: [AppComponent, CardFormComponent, InputComponent],
+  declarations: [AppComponent, CardFormComponent, InputComponent, CardComponent],
   imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
   providers: [],
   bootstrap: [AppComponent]
